71a19e68a78c5d55b4a0146d0bd770525ee06624
Frederic Ferrandis <frederic.ferrandis@openwide.fr>
2010-06-14 22:41:24 +0200

    correct bug in macro dso_get_entry

--- dso/test/main.c
+++ dso/test/main.c
@@ -6,14 +6,16 @@
 int main()
 {
 	struct dso_plugin_desc    plugin;
-	struct plugin_test  *test = NULL;
+	struct plugin_test  *test  = NULL;
+	struct plugin_test  *test2 = NULL;
+	struct plugin_test* (*ptr)(void) = NULL;
 
 	if(dso_plugin_init(&plugin, "./libplugintest.so", "get_plugintest") < 0){
 		return (0);
 	}
 
-	test = dso_plugin_get_entry(&plugin, *test);
-
+	ptr  = dso_plugin_get_entry(&plugin, ptr);
+	test = ptr(); 
 	test->display(__FILE__);
 
 	dso_plugin_exit(&plugin);
