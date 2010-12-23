f21131f08930277d935a2402c47b45f2376f84c2
Frederic Ferrandis <frederic.ferrandis@gmail.com>
2010-12-23 21:57:15 +0100

    preparation of pool memory ok

--- heap_pool/heap_pool.c
+++ heap_pool/heap_pool.c
@@ -1,219 +1,145 @@
 #include <stdint.h>
+#include <stddef.h>
+#include <string.h>
 #include <sys/types.h>
 #include <stdlib.h>
 #include <errno.h>
-
-
-#define thread_scope __thread
-#ifdef HEAP_POOL_UP
-# undef  thread_scope
-# define thread_scope
-#endif
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define __HEAP_POOL_ALLOC_STEP	(5)
 #define __HEAP_POOL_ALLOC_CHUNK_STEP	(10)
 
-static thread_scope struct heap_pool *__hp_list;
-static thread_scope struct heap_pool *__hp_last;
-static thread_scope size_t __hp_nmemb;
 
 #ifndef unlikely 
-#define unlikely(x)	(__builtin_expect((x), 0))
+# define unlikely(x)	(__builtin_expect((x), 0))
 #endif
 
 #ifndef likely 
-#define likely(x)	(__builtin_expect((x), 1))
+# define likely(x)	(__builtin_expect((x), 1))
 #endif
 
-struct heap_pool {
-	size_t   hp_nmemb;
-	size_t   hp_size;
+#ifdef DBG
+# define HEAP_POOL_DEBUG(...)  printf(__VA_ARGS__);
+#else
+# define HEAP_POOL_DEBUG(...)  do{}while(0)
+#endif
 
-	uint8_t  *hp_mem;
-	size_t   *hp_curr;
-	size_t	 *hp_index;
+
+#if  defined(_BSD_SOURCE) || _XOPEN_SOURCE >= 500
+# define heap_pool_getpagesize()	getpagesize()
+#else
+# define heap_pool_getpagesize()	sysconf(_SC_PAGESIZE)
+#endif
+
+
+struct heap_pool_desc {
+	struct   heap_pool_desc *hpd_next;
+	size_t   hpd_esize;
+	size_t   hpd_enum;
+	size_t   hpd_holesize;
+	size_t   hpd_firstfree;
+	size_t   hpd_allocsize;
+	uint8_t  *hpd_firstelem;
+	uint8_t  hpd_raw[];
 };
 
-__attribute__((constructor))
-static void __hp_private_constructor(void)
+#define __ALIGN(x, y) (((x) + (y) - 1) & ~((y) - 1))
+#define __ALIGN_PTR(addr, align) ((void*)__ALIGN((unsigned long)(addr), (align)))
+
+#ifdef   HEAP_POOL_OVERFLOW_DBG
+typedef  uint32_t chunk_extra_t;	
+# define HEAP_POOL_MAGIC 0xcacacaca
+#else
+typedef  uint8_t  chunk_extra_t;
+# define HEAP_POOL_MAGIC 0xca 
+#endif
+
+#define HEAP_CHUNK_FREE (0)
+#define HEAP_CHUNK_USED (1)
+
+void heap_pool_destroy(struct heap_pool_desc *desc, char name[])
 {
-	__hp_nmemb = __HEAP_POOL_ALLOC_STEP;
-	__hp_list  = calloc(__HEAP_POOL_ALLOC_STEP, sizeof(*__hp_list));
-	__hp_last  = __hp_list;
+	munmap(desc, desc->hpd_allocsize);
+	shm_unlink(name);
 }
 
-__attribute__((destructor))
-static void __hp_private_destructor(void)
+struct heap_pool_desc* heap_pool_create(char name[],
+					size_t const nb,
+					size_t const size,
+					size_t const align)
 {
-	if(likely(__hp_list != NULL)) {
-		free(__hp_list);
-		__hp_list = NULL;
-		__hp_last = NULL;
-	}
-}
+	size_t allocsize;
+	size_t chunksize;
+	size_t holesize;
+	size_t pagesize;
+	struct heap_pool_desc *ret = NULL;
+	int fd;
+	size_t  *walker, i;
+	uint8_t *ptr;
 
-static inline size_t __hp_nb_pool(void)
-{
-	return (size_t) (__hp_last - __hp_list);
-}
-
-static inline int __hp_pool_has_chunk_left(struct heap_pool const *const p)
-{
-	return (size_t)(p->hp_curr - p->hp_index) < p->hp_nmemb ;
-}
-
-struct heap_pool * hp_create_pool(size_t nb, size_t size)
-{
-	size_t i;
-	if(__hp_nb_pool() >= __hp_nmemb) {
-		void *tmp;
-		tmp = realloc(__hp_list, __hp_nmemb + __HEAP_POOL_ALLOC_STEP);
-		if(__builtin_expect(tmp == NULL, 0)) {
-			return ((struct heap_pool*)-ENOMEM);
-		}
-		__hp_list = tmp;
-		__hp_last = __hp_list + __hp_nmemb; 
+	if(align & 0x1) {
+		return (void*)(long)(-EINVAL);
 	}
 
-	__hp_last->hp_nmemb = nb;
-	__hp_last->hp_size  = size;
-	__hp_last->hp_mem   = calloc(nb, size);
-	__hp_last->hp_curr  = calloc(nb, size); 
-	__hp_last->hp_index = __hp_last->hp_curr;
-	for(i = 0; i < nb; ++i)
-		__hp_last->hp_curr[i] = i;
-	return __hp_last++;
-}
+	pagesize  = (size_t)heap_pool_getpagesize();
+	chunksize = size + sizeof(chunk_extra_t);
+	holesize  = __ALIGN(chunksize, align) - chunksize;
+	allocsize = sizeof(*ret) + align + nb*(sizeof(size_t) + chunksize);
+	allocsize = __ALIGN(allocsize, pagesize);
 
-static void* __hp_realloc_mem_chunk(struct heap_pool const *const p) 
-{
-	void *ret;
-	ret = realloc(p->hp_mem,
-		(p->hp_nmemb +__HEAP_POOL_ALLOC_CHUNK_STEP) * p->hp_size);
+	HEAP_POOL_DEBUG("allocsize is %zu\n", allocsize);
+	HEAP_POOL_DEBUG("holesize is %zu\n", holesize);
 
+	fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL, 0666);
+	if(unlikely(fd < 0)) {
+		return (void*)(long)(-errno);
+	}
+	ftruncate(fd, allocsize);
+
+	ret = mmap(NULL, allocsize, PROT_READ | PROT_WRITE, 
+		MAP_SHARED, fd, 0);
+	if(ret == MAP_FAILED) {
+		goto mapfailed;
+	}
+	close(fd);
+	ret->hpd_next  = NULL;
+	ret->hpd_esize = size;
+	ret->hpd_enum  = 
+		((allocsize - align - offsetof(struct heap_pool_desc, hpd_raw)) /
+		 (sizeof(size_t) + holesize + chunksize));
+
+	HEAP_POOL_DEBUG("numelem is %zu\n", ret->hpd_enum);
+
+	ret->hpd_holesize  = holesize;
+	ret->hpd_firstfree = 0;
+	walker = (size_t*)ret->hpd_raw;
+	ptr    = __ALIGN_PTR(walker + ret->hpd_enum, align);
+	HEAP_POOL_DEBUG("ptr go %p to %p\n", walker + ret->hpd_enum, ptr);
+	ret->hpd_firstelem = ptr;
+	for(i = 0; i < ret->hpd_enum; ++i, ++walker, ptr+=chunksize+holesize) {
+		*walker = i;
+		*(ptr + size) = HEAP_POOL_MAGIC | HEAP_CHUNK_FREE;
+	}
+
+	return ret;
+
+mapfailed:
+	close(fd);
 	return ret;
 }
 
-static void* __hp_realloc_mem_desc(struct heap_pool const *const p)
+int main()
 {
-	void *ret;
-	size_t nsize = 
-		(p->hp_nmemb + __HEAP_POOL_ALLOC_CHUNK_STEP) * sizeof(size_t); 
-	ret = realloc(p->hp_index, nsize);
-	return ret;
-}
+	struct heap_pool_desc * p = heap_pool_create("/plop", 30, 20, 64);
 
-static int __hp_grow_pool_mem(struct heap_pool *const hp)
-{
-	void *tmp;
-	size_t i, max;
-	size_t *walker;
-	size_t diff = hp->hp_curr - hp->hp_index;
-	tmp = __hp_realloc_mem_chunk(hp);
-	if(!tmp) 
-		return -ENOMEM;
-	hp->hp_mem = tmp;
 
-	tmp = __hp_realloc_mem_desc(hp);
-	if(!tmp)
-		return -ENOMEM;
-	hp->hp_index = tmp;
-	hp->hp_curr  = hp->hp_index + diff;
-	walker = hp->hp_index + hp->hp_nmemb; 
-	max    = hp->hp_nmemb + __HEAP_POOL_ALLOC_CHUNK_STEP;
-	for(i = hp->hp_nmemb; 
-		i < hp->hp_nmemb +  __HEAP_POOL_ALLOC_CHUNK_STEP; 
-		++i, ++walker) {
+	getchar();
 
-		*walker = i; 
-	}
-	hp->hp_nmemb = max;
-	return 0;
+	heap_pool_destroy(p, "/plop");
 
-}
-
-void* hp_alloc_mem(struct heap_pool *const hp)
-{
-	if(__builtin_expect(hp == NULL, 0)) {
-		return ((void*)-EINVAL);
-	}
-	if(!__hp_pool_has_chunk_left(hp)) {
-		int ret = __hp_grow_pool_mem(hp);
-		if(ret) {
-			return (void*)ret;
-		}
-	}
-	return &hp->hp_mem[hp->hp_size * *(hp->hp_curr++)];	
-}
-
-static inline int 
-__hp_mem_to_index(struct heap_pool const *const hp, void *const _mem, size_t *r)
-{
-	int ret = -1;
-	uint8_t const *const mem = (uint8_t*)_mem;
-	if(mem >= hp->hp_mem && mem < hp->hp_mem + *hp->hp_curr * hp->hp_size) {
-		*r  = (uint8_t*)mem - hp->hp_mem;
-		ret = 0;
-	}
-	return ret;
-}
-
-static int __hp_search_chunk(struct heap_pool *const hp, void *mem)
-{
-	size_t r, *ptr;
-	int ret;
-	int i, nb_iter;
-	ret = __hp_mem_to_index(hp, mem, &r);
-	if(ret)
-		return ret;
-
-	nb_iter = (int) (hp->hp_curr - hp->hp_index);
-
-	for(ptr = hp->hp_index, i = 0; i < nb_iter; ++ptr, ++i)
-		if(*ptr == r)
-			return i;
-	return -1;
-}
-
-void hp_free_mem(struct heap_pool *const hp, void *_mem)
-{
-	int r;
-	size_t b;
-
-	r = __hp_search_chunk(hp, _mem);
-	if(r < 0)
-		return;
-
-	--hp->hp_curr;
-	b = hp->hp_index[r];
-	hp->hp_index[r] = *(hp->hp_curr);
-	hp->hp_curr[0] = b;
-}
-
-#include <stdio.h>
-
-int main(int argc, char *const argv[])
-{
-	struct heap_pool *pool = hp_create_pool(10, 10);	
-	void *p[15];
-	int i;
-	if(pool == NULL) {
-		return 0;	
-	}
-
-	for(i = 0; i < 15; ++i) {
-		p[i] = hp_alloc_mem(pool);
-		printf("%d %p\n", i, p[i]);
-	}
-	
-	hp_free_mem(pool, p[10]);
-	hp_free_mem(pool, p[10]);
-
-	for(i = 0; i < 8; ++i)
-		hp_free_mem(pool, p[i]);
-
-	for(i = 14; i >=8; --i)
-		hp_free_mem(pool, p[i]);
 	return 0;
 }
-
